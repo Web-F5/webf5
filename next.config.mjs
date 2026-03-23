e2d7ec4157abd31ec7003a65362768f3e02e6631
Joshua Ekberg <contact@webf5.au>
2026-03-23 14:40:03 +1100

    initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
-const SITE_SUBDIR = '/demos/webf5';    // ← update this value
 
 const nextConfig = {
   output: 'export',
-  basePath: SITE_SUBDIR,
-  assetPrefix: `${SITE_SUBDIR}/`,
+
+  basePath: '',
+  assetPrefix: '',
+
   trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
+
   reactStrictMode: false,
+
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
